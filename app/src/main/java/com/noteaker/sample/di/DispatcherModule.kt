9d585b3bf987bc22db8497ae03329dc17c536803
Bao Do <bao_do@intuit.com>
2026-03-27 13:47:50 -0400

    Adding RepositoryScope

--- app/src/main/java/com/noteaker/sample/di/DispatcherModule.kt
+++ app/src/main/java/com/noteaker/sample/di/DispatcherModule.kt
@@ -5,7 +5,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Qualifier
 
 @Qualifier
@@ -19,4 +21,14 @@ object DispatcherModule {
     @Provides
     @IoDispatcher
     fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @DefaultRepositoryCoroutineScope
+    fun provideScope(@IoDispatcher dispatcher: CoroutineDispatcher): CoroutineScope {
+        return CoroutineScope(dispatcher + SupervisorJob())
+    }
 }
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultRepositoryCoroutineScope
